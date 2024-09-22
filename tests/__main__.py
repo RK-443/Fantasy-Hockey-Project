9a2709283e8ea2c80dd707f9519c1c5cf5b85e7f
RK-443 <rayan-kharroubi@hotmail.com>
2024-09-22 14:54:29 -0400

    test: parsing data with Beautiful Soup

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,1 +1,12 @@
 # main file for tests
+
+#testing grounds for requests    
+import requests
+from bs4 import BeautifulSoup
+
+URL="https://www.dailyfaceoff.com/nhl-weekly-schedule/2024-10-07" #using week of Oct 7 for url
+r=requests.get(URL)
+
+pageData= BeautifulSoup(r.content,'html5lib')
+
+print(pageData.prettify())
