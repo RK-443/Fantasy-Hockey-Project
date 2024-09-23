a1eed039876e7f9f8fd352b0962ab0905d2b93f7
RK-443 <rayan-kharroubi@hotmail.com>
2024-09-23 17:03:08 -0400

    test: parsing data from dailyfaceoff with table

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,12 +1,25 @@
 # main file for tests
 
-#testing grounds for requests    
+#-------- testing grounds for requests --------    
 import requests
 from bs4 import BeautifulSoup
 
 URL="https://www.dailyfaceoff.com/nhl-weekly-schedule/2024-10-07" #using week of Oct 7 for url
 r=requests.get(URL)
 
-pageData= BeautifulSoup(r.content,'html5lib')
+soup= BeautifulSoup(r.content,'html5lib')
 
-print(pageData.prettify())
+#print(soup.prettify()) will print the whole page
+
+#-------- testing grounds for parsing data (JS) --------  
+print(soup)
+table=soup.find('table', attrs={'class':'relative w-full table-fixed border-collapse'})
+
+data=[]
+for row in table.find_all('tr'):
+    for cell in row.find_all('td'):
+        print(cell.text)
+
+
+
+#Possible need to make library to read table
