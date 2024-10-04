694586f472d45062dfe0ec60795f175ac1a3f4a0
RK-443 <rayan-kharroubi@hotmail.com>
2024-10-04 15:15:16 -0400

    added initialization for selected week in main

--- src/nhl_schedule_app/__main__.py
+++ src/nhl_schedule_app/__main__.py
@@ -123,6 +123,7 @@ def search_team():
 #-------- main --------
 active_menu=True
 selected_team=None
+selected_week=-1
 all_schedules=[[0]*2 for _ in range(32)]     
 URL=get_url_schedule()
 page_data=get_page_data(URL)
